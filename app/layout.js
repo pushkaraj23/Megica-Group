ba8700303c8fdd63c5b65cd1639649fd522cb796
pushkaraj23 <pushkaraj232002@gmail.com>
2026-01-27 14:44:25 +0530

    added meta clarity

--- app/layout.js
+++ app/layout.js
@@ -26,15 +26,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-6EXQ5N4923"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)} gtag('js', new Date()); gtag('config',
-          'G-6EXQ5N4923');{" "}
-        </script> */}
+        <Script id="clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v7xet0xgo8");
+          `}
+        </Script>
+        
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-EGN22E922R"
           strategy="afterInteractive"
